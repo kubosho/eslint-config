3aed2366c28dcea4e155ccdbe884bc1ad0c7adc6
Shota Kubota <ta2@o2p.jp>
2022-12-08 23:35:00 +0900

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,77 @@
+module.exports = {
+  root: true,
+  env: { browser: true, node: true, es6: true, jest: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@next/next/recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".d.ts"],
+      },
+    },
+    "import/ignore": ["node_modules"],
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "always",
+      },
+    ],
+    "import/newline-after-import": "error",
+    "import/no-unresolved": [
+      "error",
+      {
+        caseSensitive: true,
+        commonjs: true,
+      },
+    ],
+    "import/no-extraneous-dependencies": "error",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-use-before-define": [
+      "error",
+      { functions: false, classes: true, variables: true },
+    ],
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+        allowHigherOrderFunctions: true,
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["*.js", "*.jsx"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+  ],
+};
