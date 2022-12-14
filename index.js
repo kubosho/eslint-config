b2858ab2dcf57a08932b6c52f3c76f788d806ddb
Shota Kubota <ta2@o2p.jp>
2022-12-14 22:43:56 +0900

    refactor(*): separate react rule for ESLint

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 module.exports = {
   root: true,
   env: { browser: true, node: true, es6: true, jest: true },
-  extends: ["eslint:recommended", "prettier", "./rules/import"],
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "./rules/import",
+    "./rules/react",
+  ],
   rules: {},
 };
