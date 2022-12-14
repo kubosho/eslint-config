7fa2f2763d092b622af254bc6dd8cb8473e1f98c
Shota Kubota <ta2@o2p.jp>
2022-12-14 20:18:48 +0900

    refactor(*): remove eslint-plugin-prettier package

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ module.exports = {
   env: { browser: true, node: true, es6: true, jest: true },
   extends: [
     "eslint:recommended",
-    "plugin:prettier/recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
     "prettier",
